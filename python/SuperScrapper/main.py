c756a19f424603122e3f400e2c67ac650b6a7a64
juan-rybczinski <jhreplay.lee@gmail.com>
2021-03-23 16:47:22 +0900

    [SuperScrapper] #4.6 Rendering Jobs!

--- python/SuperScrapper/main.py
+++ python/SuperScrapper/main.py
@@ -16,9 +16,9 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
-        from_db = db.get(word)
-        if from_db:
-            jobs = from_db
+        existing_jobs = db.get(word)
+        if existing_jobs:
+            jobs = existing_jobs
         else:
             jobs = get_jobs(word)
             db[word] = jobs
@@ -26,7 +26,7 @@ def report():
     else:
         return redirect('/')
 
-    return render_template('report.html', searchingBy=word, resultsNumber=len(jobs))
+    return render_template('report.html', searchingBy=word, resultsNumber=len(jobs), jobs=jobs)
 
 
 # @app.route('/<username>')
