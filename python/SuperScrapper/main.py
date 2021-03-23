5a12647a81d07d63571843df9d8ef1ad583a9aa9
juan-rybczinski <jhreplay.lee@gmail.com>
2021-03-23 15:39:12 +0900

    [SuperScrapper] #4.2 Dynamic URLs and Templates

--- python/SuperScrapper/main.py
+++ python/SuperScrapper/main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('SuperScrapper')
 
 
 @app.route('/')
 def home():
-    return "Hello! Wecome to my home"
+    return render_template('index.html')
+
+
+@app.route('/<username>')
+def contact(username):
+    return f'Hello {username}, how are you?'
 
 
 app.run(host='127.0.0.1')
