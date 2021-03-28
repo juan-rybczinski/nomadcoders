89990617ac7251c10aa97a79423f35354a9274e9
juan-rybczinski <jhreplay.lee@gmail.com>
2021-03-28 09:27:22 +0900

    [airbnb-clone] #6.1 Room Admin Panel part Two

--- airbnb-clone/rooms/admin.py
+++ airbnb-clone/rooms/admin.py
@@ -15,6 +15,59 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "city",
+                    "price",
+                    "address",
+                    "room_type",
+                )
+            },
+        ),
+        (
+            "Times",
+            {
+                "fields": (
+                    "check_in",
+                    "check_out",
+                    "instant_book",
+                )
+            },
+        ),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "guests",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                )
+            },
+        ),
+        (
+            "More about the Space",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_rules",
+                ),
+            },
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)},
+        ),
+    )
+
     list_display = (
         "name",
         "description",
@@ -33,6 +86,11 @@ class RoomAdmin(admin.ModelAdmin):
 
     list_filter = (
         "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
         "city",
         "country",
     )
@@ -40,6 +98,12 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = (
         "=city",
         "^host__username",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
     )
 
 
