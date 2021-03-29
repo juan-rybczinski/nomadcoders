edd46990bdf36e18234fdf634e78502fbdbc5bd3
juan-rybczinski <jhreplay.lee@gmail.com>
2021-03-29 17:50:04 +0900

    [airbnb-clone] #11.4 Using Django Paginator
    https://docs.djangoproject.com/en/3.1/topics/pagination/

--- airbnb-clone/rooms/views.py
+++ airbnb-clone/rooms/views.py
@@ -1,22 +1,15 @@
-from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    last_page = ceil(models.Room.objects.count() / page_size)
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
     return render(
         request,
         "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "last_page": last_page,
-        },
+        context={"rooms": rooms},
     )
