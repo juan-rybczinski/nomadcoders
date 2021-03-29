7c6d14f54c19f46e6585a47abdc9e9630aba91fe
juan-rybczinski <jhreplay.lee@gmail.com>
2021-03-29 17:27:04 +0900

    [airbnb-clone] #11.2 Pages List Navigation
    https://docs.djangoproject.com/en/3.1/ref/request-response/

--- airbnb-clone/rooms/views.py
+++ airbnb-clone/rooms/views.py
@@ -1,17 +1,21 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", "1"))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
+    last_page = ceil(models.Room.objects.count() / page_size)
     return render(
         request,
         "rooms/home.html",
         context={
             "rooms": all_rooms,
+            "pages": range(1, last_page + 1),
         },
     )
