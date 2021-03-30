f2d1bd9b09a585723ccc31c374bd9c9cb901f00f
juan-rybczinski <jhreplay.lee@gmail.com>
2021-03-30 13:43:23 +0900

    [airbnb-clone] #12.4 Using DetailView CBV

--- airbnb-clone/rooms/views.py
+++ airbnb-clone/rooms/views.py
@@ -1,6 +1,4 @@
-from django.views.generic import ListView
-from django.shortcuts import render
-from django.http import Http404
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -15,16 +13,5 @@ class RoomList(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(
-            request,
-            "rooms/room_detail.html",
-            {
-                "room": room,
-            },
-        )
-
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+    model = models.Room
