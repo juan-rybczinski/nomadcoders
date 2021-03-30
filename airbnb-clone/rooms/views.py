07604687e484815e4aa6a60b188175fed98a849d
juan-rybczinski <jhreplay.lee@gmail.com>
2021-03-31 05:57:03 +0900

    [airbnb-clone] #13.6 Filtering Like a Boss part Two

--- airbnb-clone/rooms/views.py
+++ airbnb-clone/rooms/views.py
@@ -31,8 +31,8 @@ def search(request):
     baths = int(request.GET.get("baths", 0))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
-    instant = request.GET.get("instant")
-    super_host = request.GET.get("super_host")
+    instant = bool(request.GET.get("instant"))
+    superhost = bool(request.GET.get("superhost"))
 
     room_types = models.RoomType.objects.all()
     amenities = models.Amenity.objects.all()
@@ -50,7 +50,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
     choices = {
         "countries": countries,
@@ -69,7 +69,37 @@ def search(request):
     if room_type != 0:
         filter_args["room_type__pk"] = room_type
 
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedroomds__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    print(filter_args)
     rooms = models.Room.objects.filter(**filter_args)
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            rooms = rooms.filter(amenities__pk=int(s_amenity))
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            rooms = rooms.filter(facilities__pk=int(s_facility))
 
     return render(
         request,
