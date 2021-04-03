16a18ee3d29be1fb01fa3d374f9a7b6602002923
juan-rybczinski <jhreplay.lee@gmail.com>
2021-04-03 11:06:06 +0900

    [airbnb-clone] #16.1 Dotenv
    https://github.com/jpadilla/django-dotenv

--- airbnb-clone/config/settings.py
+++ airbnb-clone/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,5 +148,5 @@ MEDIA_URL = "/media/"
 
 EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
