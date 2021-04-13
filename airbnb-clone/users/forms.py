a1da9b35f6fa67f0e82e0c35f7ed9531fb131f9c
juan-rybczinski <jhreplay.lee@gmail.com>
2021-04-14 08:07:47 +0900

    [airbnb-clone] #21.8 Customizing Stuff

--- airbnb-clone/users/forms.py
+++ airbnb-clone/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
 from . import models
 
 
@@ -65,3 +66,25 @@ class SignUpForm(forms.ModelForm):
         user.username = email
         user.set_password(password)
         user.save()
+
+
+class UpdatePasswordForm(PasswordChangeForm):
+
+    old_password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Current Password", "class": "btn"}
+        ),
+    )
+    new_password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "New Password", "class": "btn"}
+        ),
+    )
+    new_password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Confirm New Password", "class": "btn"}
+        ),
+    )
--- airbnb-clone/users/views.py
+++ airbnb-clone/users/views.py
@@ -223,6 +223,17 @@ class UpdateProfileView(UpdateView):
     def get_object(self, queryset=None):
         return self.request.user
 
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["first_name"].widget.attrs = {"placeholder": "First name"}
+        form.fields["last_name"].widget.attrs = {"placeholder": "Last name"}
+        form.fields["bio"].widget.attrs = {"placeholder": "Bio"}
+        form.fields["birth_date"].widget.attrs = {"placeholder": "Birth date"}
+        return form
+
 
 class UpdatePasswordView(PasswordChangeView):
+    model = models.User
     template_name = "users/update-password.html"
+    form_class = forms.UpdatePasswordForm
+    success_url = reverse_lazy("core:home")
