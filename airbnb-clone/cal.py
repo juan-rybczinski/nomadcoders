1b35633b7b2773a019ce94dadbcb9610ef9cba4e
juan-rybczinski <jhreplay.lee@gmail.com>
2021-04-25 08:30:05 +0900

    [airbnb-clone] #24.2 Room Calendar Styles

--- airbnb-clone/cal.py
+++ airbnb-clone/cal.py
@@ -3,6 +3,7 @@ import calendar
 
 class Calendar(calendar.Calendar):
     def __init__(self, year, month):
+        super().__init__(firstweekday=6)
         self.year = year
         self.month = month
         self.day_names = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
@@ -22,7 +23,7 @@ class Calendar(calendar.Calendar):
         )
 
     def get_days(self):
-        week = self.monthdays2calendar(self.year, self.month)
+        weeks = self.monthdays2calendar(self.year, self.month)
         days = []
         for week in weeks:
             for day, _ in week:
