edf05f947cf75ce6bab8d1f4aae68cef5817774b
juan-rybczinski <jhreplay.lee@gmail.com>
2021-04-25 08:50:05 +0900

    [airbnb-clone] #24.3 Finishing Calendar

--- airbnb-clone/cal.py
+++ airbnb-clone/cal.py
@@ -1,4 +1,14 @@
+from django.utils import timezone
 import calendar
+
+
+class Day:
+    def __init__(self, number, past):
+        self.number = number
+        self.past = past
+
+    def __str__(self):
+        return str(self.number)
 
 
 class Calendar(calendar.Calendar):
@@ -25,9 +35,18 @@ class Calendar(calendar.Calendar):
     def get_days(self):
         weeks = self.monthdays2calendar(self.year, self.month)
         days = []
+        now = timezone.now()
+        today = now.day
+        month = now.month
         for week in weeks:
             for day, _ in week:
-                days.append(day)
+                past = False
+                if month == self.month:
+                    if day <= today:
+                        past = True
+                new_day = Day(day, past)
+                days.append(new_day)
+
         return days
 
     def get_month(self):
